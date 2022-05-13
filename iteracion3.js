b6b1f3c07f71a00df4641698563156e6f5c24f55
melgeller <melgeller.foto@gmail.com>
2022-05-13 16:19:47 +0200

    iteracion3

--- iteracion3.js
+++ iteracion3.js
@@ -1,9 +1,13 @@
 const numbers = [1, 2, 3, 5, 45, 37, 58];
 
+let sumaTotal = 0;
+
 function sumAll(param) {
-    let sumaTotal= ""
+  for (let i = 0; i < param.length; i++) {
+sumaTotal += param[i] 
+  }
 
-    
-
-
+  console.log(sumaTotal);
 }
+
+sumAll(numbers);
---
+++ iteracion4.js
@@ -0,0 +1,5 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+
+    
+}
