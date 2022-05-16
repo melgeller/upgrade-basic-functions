34d25c2554d18bb4bd2aad04944f94a6a534c7e1
melgeller <melgeller.foto@gmail.com>
2022-05-16 17:49:26 +0200

    iteracion8

--- iteracion8.js
+++ iteracion8.js
@@ -12,7 +12,25 @@ const counterWords = [
     'code'
   ];
 
-
+  let repetitions = 0
+  let repeated = []
   
+  function repeatCounter(param) {
+      for(i = 0; i < param.length; i++){
+        for(e = 0; e < param.length; e++){
+            if(param[i]== param[e]){
+                repetitions ++ 
+            }
+        }
 
+        if (!repeated.includes(param[i])){ 
+        repeated.push(param[i], repetitions)
+         }
+        repetitions = 0
 
+    }
+  }
+
+repeatCounter(counterWords)
+
+console.log(repeated);
